146fa5b6eb9d98c6aff5ce7398836c0f78f18992
Benjamin Liet <benjamin.liet.dev@gmail.com>
2023-03-02 15:36:16 +0100

    ✨feature: Finish the post CRUD
    🖥️ wip: Need to add style to this CRUD app and add navigation guards

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name="index"),
     path('new/', views.PostCreateView.as_view(), name="new"),
     path('<int:pk>/', views.PostDetailView.as_view(), name="show"),
+    path('<int:pk>/edit', views.PostUpdateView.as_view(), name="update"),
+    path('<int:pk>/delete', views.PostDeleteView.as_view(), name="delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from posts.models import Post
 from posts.forms import PostForm
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from allauth.utils import get_user_model
 
 # Create your views here.
 class PostListView(ListView):
@@ -17,3 +18,20 @@ class PostCreateView(CreateView):
 
     def get_success_url(self):
         return reverse("posts:show", kwargs={'pk': self.object.id})
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['initial']['user'] = self.request.user
+        return kwargs
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse("posts:show", kwargs={'pk': self.object.id})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("posts:index")
